1264784422697c78a50cefcf1f76c0e25f3d8705
F8Hardin <joslhard@ut.utm.edu>
2019-07-25 09:52:58 -0500

    Blah

--- Astroneer Helper Guide/MainWindow.xaml.cs
+++ Astroneer Helper Guide/MainWindow.xaml.cs
@@ -15,9 +15,6 @@ using System.Windows.Shapes;
 
 namespace Astroneer_Helper_Guide
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         //MainWindow: Initializes everything
